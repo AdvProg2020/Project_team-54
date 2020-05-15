7ea8dccd7d5510c1ac5e65c7e17ec007653509e9
PooyaY <59162706+pooyay@users.noreply.github.com>
2020-05-15 13:29:09 +0430

    credit added + getCompanyName

--- src/main/java/model/Seller.java
+++ src/main/java/model/Seller.java
@@ -6,6 +6,7 @@ public class Seller extends Account {
     private ArrayList<Seller> allSellers = new ArrayList<>();
     private ArrayList<SellLog> sellLog;
     private ArrayList<Products> allProducts = new ArrayList<>();
+    private double credit;
 
     public Seller(String username, String name, String lastName, String email, String telephonNumber, String password,Role role,String companyName) {
         super(username,name,lastName,telephonNumber,email,password,role);
@@ -25,12 +26,16 @@ public class Seller extends Account {
         return allProducts;
     }
 
-    private void viewPersonalInfo(){
-
+    public String getCompanyName() {
+        return companyName;
     }
 
-    private void viewCompanyInformation(){
+    public double getCredit() {
+        return credit;
+    }
 
+    public void setCredit(double credit) {
+        this.credit = credit;
     }
 
     private void viewSalesHistory(){
@@ -60,5 +65,14 @@ public class Seller extends Account {
 
     }
 
-
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "companyName='" + companyName + '\'' +
+                ", allSellers=" + allSellers +
+                ", sellLog=" + sellLog +
+                ", allProducts=" + allProducts +
+                ", credit=" + credit +
+                '}';
+    }
 }
