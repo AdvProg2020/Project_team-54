c1d0a18501fad948c06e7fbe39c12beeb73c00d9
rezasoumi <59171005+rezasoumi@users.noreply.github.com>
2020-05-17 08:53:12 -0700

    ProductsMenu and ProductMenu completed

---
+++ src/main/java/view/menu/Digest.java
@@ -0,0 +1,66 @@
+package view.menu;
+
+public class Digest extends Menu {
+    public Digest(Menu parentMenu) {
+        super("Digest", parentMenu);
+        submenus.put(1, addToCart());
+        submenus.put(2, selectSeller());
+        this.setSubmenus(submenus);
+    }
+
+    @Override
+    public void execute() {
+        digest();
+        int chosenMenu = Integer.parseInt(scanner.nextLine());
+        Menu nextMenu;
+        if (chosenMenu == 1)
+            addToCart();
+        if (chosenMenu == 2)
+            selectSeller();
+        nextMenu = this.parentMenu;
+        nextMenu.show();
+        nextMenu.execute();
+    }
+
+    private void digest() {
+        //send to controller
+        //TODO
+    }
+
+    private Menu addToCart() {
+        return new Menu("Add To Cart", this) {
+            @Override
+            public void show() {
+                System.out.println("Add To Cart: ");
+            }
+
+            @Override
+            public void execute() {
+                //send to controller
+                //TODO
+            }
+        };
+    }
+
+    private Menu selectSeller() {
+        return new Menu("Select Seller", this) {
+            @Override
+            public void show() {
+                System.out.println("Select Seller: ");
+                System.out.println("Enter seller username or back to return: ");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                } else {
+                    //send to controller
+                    //TODO
+                }
+            }
+        };
+    }
+}
--- src/main/java/view/menu/ProductMenu.java
+++ src/main/java/view/menu/ProductMenu.java
@@ -7,29 +7,47 @@ public class ProductMenu extends Menu{
     public ProductMenu(Menu parentMenu) {
         super("Product Menu", parentMenu);
         HashMap<Integer, Menu> submenus = new HashMap<Integer, Menu>();
-        submenus.put(1, digest());
+        submenus.put(1, new Digest(this));
         submenus.put(2, attributes());
         submenus.put(3, compare());
         submenus.put(4, Comments());
         this.setSubmenus(submenus);
     }
-    private Menu digest() {
-        //return new menu @override
-        //TODO
-        Menu menu = null;
-        return menu;
-    }
+
     private Menu attributes() {
-        //return new menu @override
-        //TODO
-        Menu menu = null;
-        return menu;
+        return new Menu("Attributes", this) {
+            @Override
+            public void show() {
+                System.out.println("Attributes: ");
+            }
+
+            @Override
+            public void execute() {
+                //send to controller
+                //TODO
+            }
+        };
     }
     private Menu compare() {
-        //return new menu @override
-        //TODO
-        Menu menu = null;
-        return menu;
+        return new Menu("Compare With Id", this) {
+            @Override
+            public void show() {
+                System.out.println("Compare With Id: ");
+                System.out.println("Enter target productId or back to return: ");
+            }
+
+            @Override
+            public void execute() {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("back")) {
+                    this.parentMenu.show();
+                    this.parentMenu.execute();
+                } else {
+                    //send to controller
+                    //TODO
+                }
+            }
+        };
     }
     private Menu Comments() {
         //return new menu @override
