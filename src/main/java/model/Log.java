4f04cc3600dd5ac0cb39ef9f4ed8112467d442f5
PooyaY <59162706+pooyay@users.noreply.github.com>
2020-05-19 08:35:18 +0430

    LOGS -__-

--- src/main/java/model/Log.java
+++ src/main/java/model/Log.java
@@ -4,17 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public abstract class Log {
-    private int logId;
     private Date logDate;
-
-    public Log(){
-        this.logDate = new Date();
-    }
-
-    public Date getLogDate() {
-        return logDate;
-    }
-
-
 
 }
--- src/main/java/model/SellLog.java
+++ src/main/java/model/SellLog.java
@@ -1,12 +1,51 @@
 package model;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SellLog extends Log {
     private String logId;
-    private String date;
+    private Date date;
     private double amountReceived;
     private double deductedAmount;
-    private ArrayList<Good> soldProduct;
-    private String buyerName;
+    private ArrayList<Good> soldProduct = new ArrayList<>();
+    private Buyer buyerName;
+    private static ArrayList<SellLog> allSellLogs = new ArrayList<>();
+    private LogStatus status;
     // sending status
+
+    public SellLog(Buyer buyer, double amountReceived, double deductedAmount, Date date){
+        this.buyerName = buyer;
+        this.amountReceived = amountReceived;
+        this.deductedAmount = deductedAmount;
+        this.date = date;
+        allSellLogs.add(this);
+    }
+
+    public void addSoldProduct(ArrayList<Good> soldProduct) {
+        this.soldProduct.addAll(soldProduct);
+    }
+
+    public double getAmountReceived() {
+        return amountReceived;
+    }
+
+    public double getDeductedAmount() {
+        return deductedAmount;
+    }
+
+    public ArrayList<Good> getSoldProduct() {
+        return soldProduct;
+    }
+
+    public Buyer getBuyerName() {
+        return buyerName;
+    }
+
+    public static ArrayList<SellLog> getAllSellLogs() {
+        return allSellLogs;
+    }
+
+    public LogStatus getStatus() {
+        return status;
+    }
 }
