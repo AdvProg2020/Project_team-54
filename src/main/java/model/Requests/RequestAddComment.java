af9f11d3758e1597172842cfb3319873551cb289
PooyaY <59162706+pooyay@users.noreply.github.com>
2020-06-26 13:35:32 +0430

    updated

--- src/main/java/model/Requests/RequestAddComment.java
+++ src/main/java/model/Requests/RequestAddComment.java
@@ -1,5 +1,6 @@
 package model.Requests;
 
+import ScenesAndControllers.AlertBox;
 import model.Comment;
 
 public class RequestAddComment extends Request {
@@ -7,5 +8,17 @@ public class RequestAddComment extends Request {
 
     public RequestAddComment(Comment comment) {
         this.comment = comment;
+    }
+
+    @Override
+    public void denyRequest() {
+        status = RequestConfirmation.Denied;
+        Request.getAllRequests().remove(this);
+        AlertBox.display("Request denied");
+    }
+
+    @Override
+    public void acceptRequest(int id) {
+        //nemidoonm chie
     }
 }
