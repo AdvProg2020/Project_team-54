86a651c7217757f2c1bced3f1ed413e0fbd78540
PooyaY <59162706+pooyay@users.noreply.github.com>
2020-06-26 22:06:47 +0430

    little changes

--- src/main/java/model/SellLog.java
+++ src/main/java/model/SellLog.java
@@ -1,28 +1,31 @@
 package model;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 public class SellLog extends Log {
     private String logId;
     private Date date;
     private double amountReceived;
     private double deductedAmount;
-    private ArrayList<Good> soldProduct = new ArrayList<>();
-    private Buyer buyerName;
+    private ArrayList<Good> allSoldProducts = new ArrayList<>();
+    private HashMap<Good,Integer> soldProducts = new HashMap<>();
+    private String buyerName;
     private static ArrayList<SellLog> allSellLogs = new ArrayList<>();
     private LogStatus status;
     // sending status
 
-    public SellLog(Buyer buyer, double amountReceived, double deductedAmount, Date date){
-        this.buyerName = buyer;
+    public SellLog(Buyer buyer, double amountReceived, double deductedAmount, Date date, HashMap<Good,Integer> soldProducts){
+        this.buyerName = buyer.getUsername();
         this.amountReceived = amountReceived;
         this.deductedAmount = deductedAmount;
         this.date = date;
+        this.soldProducts = soldProducts;
         allSellLogs.add(this);
     }
 
     public void addSoldProduct(ArrayList<Good> soldProduct) {
-        this.soldProduct.addAll(soldProduct);
+        this.allSoldProducts.addAll(soldProduct);
     }
 
     public double getAmountReceived() {
@@ -33,11 +36,11 @@ public class SellLog extends Log {
         return deductedAmount;
     }
 
-    public ArrayList<Good> getSoldProduct() {
-        return soldProduct;
+    public ArrayList<Good> getAllSoldProducts() {
+        return allSoldProducts;
     }
 
-    public Buyer getBuyerName() {
+    public String getBuyerName() {
         return buyerName;
     }
 
