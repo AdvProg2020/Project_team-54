7ab1e9a9612c9b179c7a9c00f5836fa67bb0379e
iToxic-AF <mohammd.hht00@gmail.com>
2020-06-26 23:53:56 +0430

    ...

--- src/main/java/ScenesAndControllers/ProductsMenuController.java
+++ src/main/java/ScenesAndControllers/ProductsMenuController.java
@@ -2,12 +2,20 @@ package ScenesAndControllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import model.Good;
+
+import java.io.IOException;
 
 
 public class ProductsMenuController {
@@ -23,6 +31,7 @@ public class ProductsMenuController {
     @FXML
     TableColumn<Good, Double> ratingColumn;
 
+    public static Good selectedGood;
 
     @FXML
     private void initialize() {
@@ -39,6 +48,14 @@ public class ProductsMenuController {
         ObservableList<Good> products = FXCollections.observableArrayList();
         products.addAll(Good.allProducts);
         return products;
+    }
+
+    public void showProduct(ActionEvent event) throws IOException {
+        selectedGood = productsTable.getSelectionModel().getSelectedItem();
+        Parent login = FXMLLoader.load(getClass().getResource("SingleProductScene.fxml"));
+        Scene loginScene = new Scene(login);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(loginScene);
     }
 
 
--- src/main/java/ScenesAndControllers/singleProductSceneController.java
+++ src/main/java/ScenesAndControllers/singleProductSceneController.java
@@ -1,5 +1,41 @@
 package ScenesAndControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import ScenesAndControllers.ProductsMenuController;
+import javafx.scene.image.ImageView;
+
+
 public class singleProductSceneController {
+
+    @FXML
+    Label name;
+    @FXML
+    Label seller;
+    @FXML
+    Label brand;
+    @FXML
+    Label description;
+    @FXML
+    Label price;
+    @FXML
+    Label averageScore;
+    @FXML
+    ImageView image;
+
+    @FXML
+    private void initialize() {
+
+        name.setText(ProductsMenuController.selectedGood.getName());
+        brand.setText(ProductsMenuController.selectedGood.getBrand());
+        averageScore.setText(String.valueOf(ProductsMenuController.selectedGood.getAverageScore()));
+        seller.setText(ProductsMenuController.selectedGood.getSeller().getUsername());
+        description.setText(ProductsMenuController.selectedGood.getDescription());
+        price.setText(String.valueOf(ProductsMenuController.selectedGood.getPrice()));
+        image.setImage(ProductsMenuController.selectedGood.getImage());
+
+    }
+
 
 }
